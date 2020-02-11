8d00e103136709159f2b50363687aebfbd574d97
Martin <mendozanmartin@gmail.com>
2020-02-11 17:13:54 -0500

    add title

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Bug-Tracker</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>This is our bug tracker application</p>
       </header>
