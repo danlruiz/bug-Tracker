4ac68ed66cdf31b63e7549557cc893116a22adc3
Martin <mendozanmartin@gmail.com>
2020-02-14 14:01:35 -0500

    add draggable library

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
+import Draggable, { DraggableCore } from "react-draggable"; // Both at the same time
 
 class Card extends Component {
   render() {
     return (
       <React.Fragment>
-        <div class="card text-white bg-dark mb-3" style={{ maxWidth: "18rem" }}>
-          <div class="card-header">Header</div>
-          <div class="card-body">
-            <h5 class="card-title">Dark card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+        <Draggable>
+          <div
+            className="card text-white bg-dark mb-3"
+            style={{ maxWidth: "18rem", cursor: "pointer" }}
+          >
+            <div className="card-header">Header</div>
+            <div className="card-body">
+              <h5 className="card-title">Dark card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
           </div>
-        </div>
+        </Draggable>
       </React.Fragment>
     );
   }
